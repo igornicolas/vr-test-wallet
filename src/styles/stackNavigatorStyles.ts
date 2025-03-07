e3143d62e77c5e745ba4d75749fcd22669427812
igornicolas <igornicolas33@gmail.com>
2025-03-07 07:51:21 -0300

    delete unused file

--- src/styles/stackNavigatorStyles.ts
+++
@@ -1,9 +0,0 @@
-import { StyleSheet } from 'react-native';
-
-export const STACK_STYLES = StyleSheet.create({
-    container: {
-        alignItems: 'center',
-        justifyContent: 'center',
-        flex: 1,
-    },
-});
