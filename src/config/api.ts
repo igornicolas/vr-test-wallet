c67d76cb607c84b5332a15cceb3507256d344fcb
igornicolas <igornicolas33@gmail.com>
2025-03-07 07:30:34 -0300

    fix api baseUrl check

--- src/config/api.ts
+++ src/config/api.ts
@@ -2,8 +2,8 @@ import axios from 'axios';
 import * as Device from 'expo-device';
 
 const baseURL = Device.isDevice
-    ? 'http://192.168.15.10:3000/'
-    : 'http://192.168.15.10:3000/';
+    ? 'http://localhost:3000/'
+    : 'http://10.0.2.2:3000/';
 
 export const api = axios.create({
     baseURL,
